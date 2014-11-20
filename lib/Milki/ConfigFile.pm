160b73da2105b591a6fc6ae4af1ca885e48c23f2
Yongbin Yu <yongbin.yu@gmail.com>
2014-11-20 12:21:33 +0900

    Making ConfigFile - Milki::Util
    M::ConfigFile을 좀 더 진행합니다. 이번에는 Milki::Util 모듈을 호출하기
    때문에 해당 모듈을 확인했습니다.
    
    비교적 이 모듈은 쓰임이 명확해 보입니다.
    
    - string_is_empty()
      이 함수는 해당 문자열이 '정의되어있는지'와 '값이 있는지'를 같이
      확인하는 함수입니다.
    - english_list()
      영어단어들을 인자로 받아서 "a and b" 혹은 "a,b,c and b" 형태의
      문자열로 표현해주는 함수입니다.

--- lib/Milki/ConfigFile.pm
+++ lib/Milki/ConfigFile.pm
@@ -8,6 +8,11 @@ use autodie qw( :all );
 use Config::INI::Reader;
 use Path::Class;
 use Milki::Types qw( Dir File HashRef Maybe );
+use Milki::Util qw( string_is_empty );
+
+use Moose;
+use MooseX::StrictConstructor;
+
 
 # ABSTRACT: Low-level interface to the config file
 
