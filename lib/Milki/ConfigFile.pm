ffe769c049dadea1a633a4e0d1a1fca054858c8f
Yongbin Yu <yongbin.yu@gmail.com>
2014-11-20 12:35:26 +0900

    Complete Milki::ConfigFile
    Milki::ConfigFile을 완성합니다.
    
    이 모듈은 설정파일 자체를 의미하는 'file'과 그 설정파일로 부터
    Config::INI::Reader를 통해서 읽은 해쉬를 의미하는 'raw_data',
    홈디렉토리를 의미하는 '_home_dir' 속성으로 구성되어있습니다.
    
    'file'의 생성자는 Milki가 지원하는 다양한 설정파일의 위치에 대해 처리한
    뒤 찾을수 있는 첫번째 milki.conf 파일을 돌려줍니다.
    
    'raw_data'의 생성자는 'file'을 결과를 Config::INI::Reader로 읽은
    해쉬(hash)형태로 설정의 내용을 돌려줍니다.

--- lib/Milki/ConfigFile.pm
+++ lib/Milki/ConfigFile.pm
@@ -13,6 +13,61 @@ use Milki::Util qw( string_is_empty );
 use Moose;
 use MooseX::StrictConstructor;
 
+has file => (
+    is      => 'rw',
+    isa     => Maybe [File],
+    lazy    => 1,
+    builder => '_build_file',
+    writer  => '_set_file',
+    clearer => '_clear_file',
+);
+
+has raw_data => (
+    is      => 'rw',
+    isa     => HashRef,
+    lazy    => 1,
+    builder => '_build_raw_data',
+    writer  => '_set_raw_data',
+    clearer => '_clear_raw_data',
+);
+
+has _home_dir => ( is => 'ro', isa => Dir, init_arg => 'home_dir', );
+
+sub _build_file {
+    my $self = shift;
+
+    if ( !string_is_empty( $ENV{MILKI_CONFIG} ) ) {
+        die
+            "Nonexistent config file in MILKI_CONFIG env var: $ENV{MILKI_CONFIG}"
+            unless -f $ENV{MILKI_CONFIG};
+
+        return file( $ENV{MILKI_CONFIG} );
+    }
+
+    return if $ENV{MILKI_CONFIG_TESTING};
+
+    my @dirs = dir('/etc/milki');
+    push @dirs, $self->_home_dir()->subdir( '.milki', 'etc' )
+        if $> && $self->_home_dir();
+
+    for my $dir (@dirs) {
+        my $file = $dir->file('milki.conf');
+
+        return $file if -f $file;
+    }
+
+    return;
+}
+
+sub _build_raw_data {
+    my $self = shift;
+
+    my $file = $self->file() or return {};
+
+    return Config::INI::Reader->read_file($file) || {};
+}
+
+__PACKAGE__->meta()->make_immutable;
 
 # ABSTRACT: Low-level interface to the config file
 
