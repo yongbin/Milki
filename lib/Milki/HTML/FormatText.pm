83534ba00a08133ccff2d2c6d7b1a5e56dd3e026
Yongbin Yu <yongbin.yu@gmail.com>
2014-12-07 23:02:48 +0900

    Complete Milki::HTML::FormatText
    Milki::Email은 Milki::HTML::FormatText에 의존성을 가지고 있어서
    구현했습니다.
    
    Milki::HTML::FormatText은 HTML::FormatText을 상속해서 구현한 모듈이며,
    HTML::FormatText는 HTML::Formatter를 기반으로 만들어졌습니다.
    
    HTML::FormatText는 HTML문서에서 서식부분을 모두 제거하고 텍스트 본문만을
    추출해 줍니다. 그 밖에 Milki::HTML::FormatText 에서는 아래
    HTML::Formatter 모듈에서 설명된 동작을 활용해서 'a' 테그의 href 속성을
    추가적으로 표현해주는 내용을 a_start, a_end 함수를 통해서
    구현했습니다.(예를 들어 <a href="http://google.com">Google</a>의 경우
    "Google (google.com)" 으로 변환
    
    >     HTML::Formatter is a base class for classes that take HTML and format it
    >     to some output format. When you take an object of such a base class and
    >     call "$formatter-"format( $tree )> with an HTML::TreeBuilder (or
    >     HTML::Element) object, they return the appropriately formatted string
    >     for the input HTML.
    
    간단한 코드이기 때문에 t/Milki/HTML/FormatText.t 도 같이 완성시켰습니다.
    
    > $ prove -l -v t/Milki/HTML/FormatText.t
    > t/Milki/HTML/FormatText.t ..
    > ok 1 - simple paragraph with em tag
    > ok 2 - paragraph with a link
    > ok 3 - paragraph with an anchor
    > 1..3
    > ok
    > All tests successful.
    > Files=1, Tests=3,  0 wallclock secs ( 0.03 usr  0.00 sys +  0.08 cusr  0.01 csys =  0.12 CPU)
    > Result: PASS

---
+++ lib/Milki/HTML/FormatText.pm
@@ -0,0 +1,33 @@
+package Milki::HTML::FormatText;
+
+use strict;
+use warnings;
+
+use base 'HTML::FormatText';
+
+# If these subs don't return true, the formatter won't recurse into the node
+# for text/etc.
+
+sub a_start {
+    my $self = shift;
+    my $node = shift;
+
+    $self->{uri_for_a} = $node->attr('href');
+
+    return 1;
+}
+
+sub a_end {
+    my $self = shift;
+    my $node = shift;
+
+    $self->out( ' (' . $self->{uri_for_a} . ')' ) if $self->{uri_for_a};
+
+    delete $self->{uri_for_a};
+
+    return 1;
+}
+
+1;
+
+# ABSTRACT: A subclass of HTML::FormatText that also handles links
