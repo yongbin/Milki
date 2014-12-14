0af4f635551a12fb12a25261010bbbd9be260b48
Yongbin Yu <yongbin.yu@gmail.com>
2014-12-14 21:41:44 +0900

    Complete M::Role::Schema::URIMaker
    - URIMaker는 MooseX::Params::Validate를 이용해서 인자를 확인하며, uri()를 제공합니다.
    - URIMaker는 M::URI를 사용해서 내부 리소스를 적절한 URI형식으로 변환합니다.
    - M::URI는 URI::FromHash를 사용해서 'path', 'query', 'fragment'등을 URI 구성요소들을
      해쉬(Hash)형태로 받아서 처리합니다.

---
+++ lib/Milki/Role/Schema/URIMaker.pm
@@ -0,0 +1,66 @@
+package Milki::Role::Schema::URIMaker;
+
+use strict;
+use warnings;
+use namespace::autoclean;
+
+use MooseX::Params::Validate qw( validate );
+use Milki::Types qw( Bool HashRef Str );
+use Milki::Util qw( string_is_empty );
+use Milki::URI qw( dynamic_uri );
+
+use Moose::Role;
+
+#requires_attr_or_method (??) 'domain';
+
+requires '_base_uri_path';
+
+sub uri {
+
+    # MX::P::V doesn't handle class methods
+    my $self = shift;
+
+    my %p = validate(
+        \@_,
+        view      => { isa => Str,     optional => 1 },
+        fragment  => { isa => Str,     optional => 1 },
+        query     => { isa => HashRef, default  => {} },
+        with_host => { isa => Bool,    default  => 0 },
+    );
+
+    my $path = $self->_base_uri_path();
+    unless ( string_is_empty( $p{view} ) ) {
+        $path .= q{/} unless $path =~ m{/$};
+        $path .= $p{view};
+    }
+
+    $self->_make_uri(
+        path      => $path,
+        fragment  => $p{fragment},
+        query     => $p{query},
+        with_host => $p{with_host},
+    );
+}
+
+sub _make_uri {
+    my $self = shift;
+    my %p    = @_;
+
+    delete $p{fragment} if string_is_empty( $p{fragment} );
+
+    return dynamic_uri( $self->_host_params_for_uri( delete $p{with_host} ),
+        %p, );
+}
+
+sub _host_params_for_uri {
+    my $self = shift;
+
+    return unless $_[0];
+
+    return ( %{ $self->domain()->uri_params() },
+        ( $ENV{SERVER_PORT} ? ( port => $ENV{SERVER_PORT} ) : () ) );
+}
+
+1;
+
+# ABSTRACT: Adds an $object->uri() method
