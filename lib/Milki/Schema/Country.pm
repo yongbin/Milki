cbb583497b14ff483f3d04e331001cb8cc1d9c5e
Yongbin Yu <yongbin.yu@gmail.com>
2014-12-20 15:52:22 +0900

    Import lib/Milki/Schema/Country.pm
    >               Table "public.Country"
    >    Column    |          Type          | Modifiers
    > -------------+------------------------+-----------
    >  iso_code    | character(2)           | not null
    >  name        | character varying(255) | not null
    >  locale_code | character varying(20)  | not null
    > Indexes:
    >     "Country_pkey" PRIMARY KEY, btree (iso_code)
    >     "Country_name_key" UNIQUE CONSTRAINT, btree (name)
    > Check constraints:
    >     "valid_iso_code" CHECK (iso_code <> ''::bpchar)
    >     "valid_name" CHECK (name::text <> ''::text)
    > Foreign-key constraints:
    >     "Country_locale_code" FOREIGN KEY (locale_code) REFERENCES "Locale"(locale_code) ON UPDATE CASCADE ON DELETE RESTRICT
    > Referenced by:
    >     TABLE ""TimeZone"" CONSTRAINT "TimeZone_iso_code" FOREIGN KEY (iso_code) REFERENCES "Country"(iso_code) ON UPDATE CASCADE ON DELETE RESTRICT
    
    - 'iso_code'는 2자리 iso 국가 코드입니다.
    - 'name'은 국가 이름입니다.
    - 'locale_code'는 로케일로 Locale 테이블의 FK입니다.
    - 각 국가는 여러 타임존에 속할수 있습니다.
    - 각 국가는 하나의 로케일을 가집니다

---
+++ lib/Milki/Schema/Country.pm
@@ -0,0 +1,48 @@
+package Milki::Schema::Country;
+
+use strict;
+use warnings;
+use namespace::autoclean;
+
+use Milki::Schema;
+
+use Fey::ORM::Table;
+
+my $Schema = Milki::Schema->Schema();
+
+{
+    has_policy 'Milki::Schema::Policy';
+
+    has_table( $Schema->table('Country') );
+
+    has_one( $Schema->table('Locale') );
+
+    has_many time_zones => (
+        table => $Schema->table('TimeZone'),
+        order_by =>
+            [$Schema->table('TimeZone')->column('display_order'), 'ASC'],
+    );
+}
+
+sub CreateDefaultCountries {
+    my $class = shift;
+
+    my @countries
+        = ( ['us', 'United States', 'en_US'], ['ca', 'Canada', 'en_CA'], );
+
+    for my $country (@countries) {
+        next if $class->new( iso_code => $country->[0] );
+
+        $class->insert(
+            iso_code    => $country->[0],
+            name        => $country->[1],
+            locale_code => $country->[2],
+        );
+    }
+}
+
+__PACKAGE__->meta()->make_immutable();
+
+1;
+
+# ABSTRACT: Represents a country
