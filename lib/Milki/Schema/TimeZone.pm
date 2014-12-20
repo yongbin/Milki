c16466b5e29325a989c7a55736294a082279fcb1
Yongbin Yu <yongbin.yu@gmail.com>
2014-12-20 15:43:55 +0900

    Import lib/Milki/Schema/TimeZone.pm
    > Table "public.TimeZone"
    >     Column     |          Type          | Modifiers
    > ---------------+------------------------+-----------
    >  olson_name    | character varying(255) | not null
    >  iso_code      | character(2)           | not null
    >  description   | character varying(100) | not null
    >  display_order | integer                | not null
    > Indexes:
    >     "TimeZone_pkey" PRIMARY KEY, btree (olson_name)
    > Check constraints:
    >     "valid_description" CHECK (description::text <> ''::text)
    >     "valid_display_order" CHECK (display_order > 0)
    >     "valid_iso_code" CHECK (iso_code <> ''::bpchar)
    >     "valid_olson_name" CHECK (olson_name::text <> ''::text)
    > Foreign-key constraints:
    >     "TimeZone_iso_code" FOREIGN KEY (iso_code) REFERENCES "Country"(iso_code) ON UPDATE CASCADE ON DELETE RESTRICT
    
    - 'olson_name'은 'America/New_York' 과 같은 시간대 이름
    - 각각의 시간대는 하나의 국가에 속한다.
    - 'description'은 'East Coast'와 같은 시간대 이름입니다.
    - 'display_order'는 시간대의 표시 순서입니다.
    - CreateDefaultZones() 함수를 호출하면 us와 ca의 시간대를 입력합니다.

---
+++ lib/Milki/Schema/TimeZone.pm
@@ -0,0 +1,67 @@
+package Milki::Schema::TimeZone;
+
+use strict;
+use warnings;
+use namespace::autoclean;
+
+use Fey::Object::Iterator::FromSelect;
+use Milki::Schema;
+
+use Fey::ORM::Table;
+
+my $Schema = Milki::Schema->Schema();
+
+{
+    has_policy 'Milki::Schema::Policy';
+
+    has_table( $Schema->table('TimeZone') );
+
+    has_one( $Schema->table('Country') );
+}
+
+sub CreateDefaultZones {
+    my $class = shift;
+
+    my %zones = (
+        us => [
+            ['America/New_York',      'East Coast'],
+            ['America/Chicago',       'Central'],
+            ['America/Denver',        'Mountain'],
+            ['America/Los_Angeles',   'West Coast'],
+            ['America/Anchorage',     'Alaska'],
+            ['Pacific/Honolulu',      'Hawaii'],
+            ['America/Santo_Domingo', 'Puerto Rico'],
+        ],
+
+        ca => [
+            ['America/Halifax',   'Halifax and New Brunswick'],
+            ['America/Toronto',   'Ontario and Quebec'],
+            ['America/Winnipeg',  'Manitoba'],
+            ['America/Regina',    'Saskatchewan'],
+            ['America/Edmonton',  'Alberta'],
+            ['America/Vancouver', 'British Columbia'],
+            ['America/St_Johns',  q{St. John's}],
+        ],
+    );
+
+    for my $iso_code ( keys %zones ) {
+        my $order = 1;
+
+        for my $zone ( @{ $zones{$iso_code} } ) {
+            next if $class->new( olson_name => $zone->[0] );
+
+            $class->insert(
+                olson_name    => $zone->[0],
+                iso_code      => $iso_code,
+                description   => $zone->[1],
+                display_order => $order++,
+            );
+        }
+    }
+}
+
+__PACKAGE__->meta()->make_immutable();
+
+1;
+
+# ABSTRACT: Represents a time zone
