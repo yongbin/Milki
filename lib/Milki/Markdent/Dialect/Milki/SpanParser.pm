58f9850f8cc3ba3e581ac531dcb0a2a0ba2d2e96
Yongbin Yu <yongbin.yu@gmail.com>
2014-12-20 19:56:57 +0900

    Complete Milki::Markdent::Dialect::Milki
    이 부분을 이해하기 위해서는 Markdent와 내부 구조에 대해서 이해해야
    합니다.
    
    Markdent는 Markdown을 위한 이벤트 기반의 파서 도구 모음입니다. 저자의
    설명에 따르면 Markdent는 기존 Markdown 도구들이 파서와 (HTML)변환기가
    합쳐져 있는 상태로 구현된 경우가 많기때문에, Markdown 자체를 확장하기가
    어려운데 Markdent는 SAX와 유사한 이벤트 기반의 파서형태로 Markdown
    처리와 문법을 분리해서 좀 더 유연하고 확장가능한 형태로 만든 도구입니다.
    
    Markdent는 방언(Dialect) 이라는 이름으로 Markdown 문법(그리고 문법에
    대한 파서)를 정의합니다. John Gruber가 최초로 만들어 공개한 Markdown
    문법은 Markdent::Dialect::Standard, David Wheeler가 제안한 확장을 구현한
    문법을 Markdent::Dialect::Theory, Github 서비스에서 제공하는 GitHub
    Flavored Markdown 문법을 Markdent::Dialect::GitHub으로 제공하는
    식입니다.
    
    각각의 Dialect은 BlockParser와 SpanParser로 구성되어있는데,
    BlockParser는 목록, 문단, 인용구 같은 덩어리를 나누는 문법이며 그 결과를
    SpanParser로 전달합니다. SpanParser는 강조, 링크와 같은 세부 문법을
    다루는 문법입니다.
    
    그리고 Markdent는 Dialect이 가진 Parser는 Parse를 한 시점에 그 결과를
    Hander에게 전달하는 방식으로 동작합니다.
    
    즉 도식으로 정리하면 다음과 같습니다.
    
    >		   Parser	--(event)--> Handler
    >	  	     /\
    >		    /  \
    >		   /    \
    >		  /      \
    >	    Block -> Span
    
    Milki는 Markdent::Dialect::Milki라는 별도의 방언을 만들고 이 방언을
    처리하기 위한 BlockParser와 SpanParser를 구현합니다. 이 파서들은
    기본적으로 Markdent::Dialect::Theory 상속해서 구현하는데, BlockParser는
    완전히 동일하게 동작하며, SpanParser에서는 위키 페이지에 대한
    링크와,파일링크, 페이지 링크에 대한 문법을 확장하고 각각의 문법을
    SpanParser에서 정규식으로 정의하고, 확장된 문법을 통해 생성될 이벤트에
    대한 Handler를 각각 Markdent::Event::FileLink,
    Markdent::Event::ImageLink, Markdent::Event::WikiLink에서 구현합니다.
    
    Markdent에 대한 좀 더 자세한 사항은 아래 문서를 참조하면 됩니다.
    
    http://search.cpan.org/~drolsky/Markdent-0.24/lib/Markdent/Manual.pod

---
+++ lib/Milki/Markdent/Dialect/Milki/SpanParser.pm
@@ -0,0 +1,171 @@
+package Milki::Markdent::Dialect::Milki::SpanParser;
+
+use strict;
+use warnings;
+use namespace::autoclean;
+
+use List::AllUtils qw( insert_after_string );
+use Milki::Markdent::Event::FileLink;
+use Milki::Markdent::Event::ImageLink;
+use Milki::Markdent::Event::WikiLink;
+
+use Moose;
+use MooseX::SemiAffordanceAccessor;
+use MooseX::StrictConstructor;
+
+extends 'Markdent::Dialect::Theory::SpanParser';
+
+sub _possible_span_matches {
+    my $self = shift;
+
+    my @look_for = $self->SUPER::_possible_span_matches(@_);
+
+    # inside code span
+    return @look_for if @look_for == 1;
+
+    for my $val (qw( image_link file_link wiki_link )) {
+        insert_after_string 'code_start', $val, @look_for;
+    }
+
+    return @look_for;
+}
+
+# More or less stolen from Text::Markdown
+my $nested_brackets;
+$nested_brackets = qr{
+    (?>                                 # Atomic matching
+       [^\[\]]+                           # Anything other than brackets
+       |
+       \[
+         (??{ $nested_brackets })        # Recursive set of nested curlies
+       \]
+    )*
+}x;
+
+sub _match_wiki_link {
+    my $self = shift;
+    my $text = shift;
+
+    return unless ${$text} =~ / \G
+                                (?:
+                                  \[
+                                  ($nested_brackets)
+                                  \]
+                                )?
+                                \(\(
+                                (.+?)
+                                \)\)
+                              /xmgc;
+
+    my %p = ( link_text => $2 );
+    $p{display_text} = $1 if defined $1;
+
+    my $event = $self->_make_event( 'Milki::Markdent::Event::WikiLink' => %p );
+
+    $self->_markup_event($event);
+
+    return 1;
+}
+
+sub _match_file_link {
+    my $self = shift;
+    my $text = shift;
+
+
+    return unless ${$text} =~ / \G
+                                (?:
+                                  \[
+                                  ($nested_brackets)
+                                  \]
+                                )?
+                                {{
+                                \s*
+                                file:
+                                \s*
+                                ([^}]+)
+                                \s*
+                                }}
+                              /xmgc;
+
+    my %p = ( link_text => $2 );
+    $p{display_text} = $1 if defined $1;
+
+    my $event = $self->_make_event( 'Milki::Markdent::Event::FileLink' => %p );
+
+    $self->_markup_event($event);
+
+    return 1;
+}
+
+sub _match_image_link {
+    my $self = shift;
+    my $text = shift;
+
+    return unless ${$text} =~ / \G
+                                {{
+                                \s*
+                                image:
+                                \s*
+                                ([^}]+)
+                                \s*
+                                }}
+                              /xmgc;
+
+    my %p = ( link_text => $1 );
+
+    my $event
+        = $self->_make_event( 'Milki::Markdent::Event::ImageLink' => %p );
+
+    $self->_markup_event($event);
+
+    return 1;
+}
+
+sub _match_plain_text {
+    my $self = shift;
+    my $text = shift;
+
+    my $escape_re = $self->_escape_re();
+
+    # Note that we're careful not to consume any of the characters marking the
+    # (possible) end of the plain text. If those things turn out to _not_ be
+    # markup, we'll get them on the next pass, because we always match at
+    # least one character, so we should never get stuck in a loop.
+    return unless ${$text} =~ /\G
+                     ( .+? )              # at least one character followed by ...
+                     (?=
+                       $escape_re
+                       |
+                       \*                 #   possible span markup
+                       |
+                       _
+                       |
+                       \p{SpaceSeparator}* \`
+                       |
+                       \(\(               #   possible wiki link
+                       |
+                       \{\{               #   possible wiki command
+                       |
+                       !?\[               #   possible image or link
+                       |
+                       < [^>]+ >          #   an HTML tag
+                       |
+                       &\S+;              #   an HTML entity
+                       |
+                       \z                 #   or the end of the string
+                     )
+                    /xgcs;
+
+    $self->_print_debug("Interpreting as plain text\n\n[$1]\n")
+        if $self->debug();
+
+    $self->_save_span_text($1);
+
+    return 1;
+}
+
+__PACKAGE__->meta()->make_immutable();
+
+1;
+
+# ABSTRACT: Parses span-level markup for the Milki Markdown dialect
---
+++ lib/Milki/Markdent/Event/ImageLink.pm
@@ -0,0 +1,20 @@
+package Milki::Markdent::Event::ImageLink;
+
+use strict;
+use warnings;
+use namespace::autoclean;
+
+use Markdent::Types qw( Str );
+
+use Moose;
+use MooseX::StrictConstructor;
+
+has link_text => ( is => 'ro', isa => Str, required => 1, );
+
+with 'Markdent::Role::Event';
+
+__PACKAGE__->meta()->make_immutable();
+
+1;
+
+# ABSTRACT: Represents a link to an image (to be embedded in the page)
